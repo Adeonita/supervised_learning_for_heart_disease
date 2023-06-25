f573d655fd12086419dfb7658d8c456e306d5dd5
adeonita <adeonita.sousa@gmail.com>
2023-06-25 15:56:18 -0300

    remove index file

--- index.py
+++
@@ -1,9 +0,0 @@
-"""entrypoint"""
-from src.database import Dataset
-
-data = Dataset('heart.csv')
-dinamic_train_data = data.get_dinamically_train_data()
-dinamic_test_data = data.get_dinamically_test_data()
-
-static_train_data = data.get_statically_train_data()
-static_test_data = data.get_statically_test_data()
