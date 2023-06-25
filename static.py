27285acfab59782a07fe2feab38bb5e09a229c47
adeonita <adeonita.sousa@gmail.com>
2023-06-25 14:58:08 -0300

    feat[static]: add file to test dinamic datas

---
+++ static.py
@@ -0,0 +1,21 @@
+"""test to static values"""
+from src.database import Dataset
+from src.knn import Knn
+
+data = Dataset('heart.csv')
+knn = Knn(data)
+
+static_x_train_data = data.fit_transform_statically_x_train_data()
+static_y_train_data = data.get_statically_y_train_data()
+
+static_x_test_data = data.fit_transform_statically_x_test_data()
+static_y_test_data = data.get_statically_y_test_data()
+
+knn.classifier(static_x_train_data, static_y_train_data)
+y_pred = knn.test_prevision(static_x_test_data)
+
+acuracy = knn.get_acuracy(static_y_test_data, y_pred)
+error_rate = knn.get_error_rate(static_x_test_data, y_pred)
+
+print(f"Acuracia: {acuracy} ≈ {round(acuracy,2)}")
+print(f"Taxa de erro: {error_rate} ≈ {round(error_rate,2)}")
