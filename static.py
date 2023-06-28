f58cf5925b8772f5b23de5dd3099ba995c5788bd
adeonita <adeonita.sousa@gmail.com>
2023-06-27 21:48:43 -0300

    fix: fix get error parameter

--- static.py
+++ static.py
@@ -15,7 +15,7 @@ knn.classifier(static_x_train_data, static_y_train_data)
 y_pred = knn.test_prevision(static_x_test_data)
 
 acuracy = knn.get_acuracy(static_y_test_data, y_pred)
-error_rate = knn.get_error_rate(static_x_test_data, y_pred)
+error_rate = knn.get_error_rate(static_y_test_data, y_pred)
 
 print(f"Acuracia: {acuracy} ≈ {round(acuracy,2)}")
 print(f"Taxa de erro: {error_rate} ≈ {round(error_rate,2)}")
